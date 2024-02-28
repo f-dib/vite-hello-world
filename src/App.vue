f59d55c72795e3b86335c2a9130576ea3097aef2
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-28 16:20:01 +0100

    exercise completed

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <script>
+import AppTitle from './components/AppTitle.vue';
 
+export default {
+  components: {
+    AppTitle
+  },
+}
 </script>
 
 
 <template>
-
+  <div class="my_title">
+    <AppTitle></AppTitle>
+  </div>
 </template>
 
 
 <style>
-
+  .my_title {
+    height: 100vh;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'AppTitle',
+}
+</script>
+
+
+<template>
+    <h1>Quante repositories di Hello World farai?</h1>
+    <div>
+        <img class="my_width" src="../assets/28fncn.jpg" alt="">
+    </div>
+</template>
+
+
+<style>
+    h1 {
+        color: red;
+        font-size: 30px;
+    }
+
+    .my_width {
+        width: 50vw;
+    }
+</style>
